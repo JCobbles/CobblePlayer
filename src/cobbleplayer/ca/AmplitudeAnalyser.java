d684ce3d5b1eef73cc57eaa1274e2a9639feff6c
mossja <mossja@K2-8.ucs.org.uk>
2014-03-24 16:38:59 +0000

    Started analysis

--- src/cobbleplayer/ca/AmplitudeAnalyser.java
+++ src/cobbleplayer/ca/AmplitudeAnalyser.java
@@ -5,23 +5,50 @@
  */
 package cobbleplayer.ca;
 
+import cobbleplayer.utilities.ModalDialog;
+import java.util.ArrayList;
+import java.util.List;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+
 /**
  *
  * @author Jacob
  */
 public class AmplitudeAnalyser implements Runnable {
-    short[] amplitudes;
+
+    short[] mix;
+
     public AmplitudeAnalyser(short[] amplitudes) {
-        this.amplitudes = amplitudes;
+        mix = amplitudes;
     }
 
     @Override
     public void run() {
+        short step_size = 1000;
+        short lower = 50, upper = 50;
+
+        if (mix.length < 1000) {
+            Button b = new Button("OK");
+            b.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent t) {
+                    ModalDialog.exit();
+                }
+            });
+            List<Button> bs = new ArrayList<>();
+            bs.add(b);
+            new ModalDialog("Error", "Song is not long enought (length of mix < step size)", bs);
+        } else {
+
+        }
         /**
-         * start with some step size and threshold limits
-         * start with first amplitude
-         * add step size, if the next amplitude is within the limits of the start then continue with step size
-         * if more than five steps have gone through with the same step size add to a list with the amplitude
+         * start with some step size and threshold limits start with first
+         * amplitude add step size, if the next amplitude is within the limits
+         * of the start then continue with step size if more than five steps
+         * have gone through with the same step size add to a list with the
+         * amplitude
          */
     }
 
